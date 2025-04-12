4afda5b0e99b0e74485e7ab22b4d6bf2d05e76ca
juanPalalacios08 <juandiegopalacios08mmm@gmail.com>
2025-04-12 12:29:41 -0500

    Agrego el challenge2

---
+++ challenge2/src/FirstApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const FistApp = () =>{
+    return(
+        <div>
+            <h1>title</h1>
+            <span>10</span>
+        </div>
+    )
+}
+
+export default FistApp
